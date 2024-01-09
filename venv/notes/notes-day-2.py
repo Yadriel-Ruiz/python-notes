0a70e58308dfc204b73481f00b8343c04a3d7fd0
Yadriel <147550067+Yadriel-Ruiz@users.noreply.github.com>
2024-01-09 09:02:42 -0600

    Initial commit

---
+++ venv/notes/notes-day-2.py
@@ -0,0 +1,8 @@
+# Tools and Resources
+# Notion - https://www.notion.so/
+#Obsidian - https://obsidian.md/
+#Evernote - https://evernote.com/
+#Excalidraw - https://excalidraw.com/ (Diagramming)
+#Anki - https://apps.ankiweb.net/ (Vocabulary)
+
+# print("Hello World")
