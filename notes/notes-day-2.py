69f98bf36dec3d5a6e47a783427dd98d77cf6b9c
Yadriel <147550067+Yadriel-Ruiz@users.noreply.github.com>
2024-01-09 10:44:06 -0600

    morning notes

--- notes/notes-day-2.py
+++ notes/notes-day-2.py
@@ -1,10 +1,106 @@
 # Tools and Resources
 # Notion - https://www.notion.so/
-#Obsidian - https://obsidian.md/
-#Evernote - https://evernote.com/
-#Excalidraw - https://excalidraw.com/ (Diagramming)
-#Anki - https://apps.ankiweb.net/ (Vocabulary)
+# Obsidian - https://obsidian.md/
+# Evernote - https://evernote.com/
+# Excalidraw - https://excalidraw.com/ (Diagramming)
+# Anki - https://apps.ankiweb.net/ (Vocabulary)
 
 # print("Hello World")
 #  Python is a program installed into your computer
 # use the terminal in order to run python
+# when running python you run it using terminal
+# find the location of the file you want to run
+# use ls in order to list the files in the directory
+# use cd in order to change directories
+# use pwd in order to print the working directory
+# use python name-of-file.py in order to run the file
+
+# if you run input() in python it will wait for you to type something
+# if you run print() in python it will print something
+# if you run print(input()) in python it will print what you type
+# you can use if statements in python
+# using char in python will allow you to use a single character
+# using int in python will allow you to use a number
+# using str in python will allow you to use a string
+# using float in python will allow you to use a decimal number
+# using bool in python will allow you to use a boolean
+# using input in python will allow you to use a string
+
+
+# Examples of using input and print **********************************************
+# name = input("What is your name? ")
+#
+# print("Hello " + name + "!")
+
+# name = input("What is your name? ")
+# first_number = input("Give me a number: ")
+# second_number = input("Give me another number: ")
+# sum = int(first_number) + int(second_number)
+
+# when printing a string and a number you need to convert the number to a string
+# you can convert a number to a string by using str(number)
+# print("The sum of " + first_number + " and " + second_number + " is " + str(sum) + ".")
+# *********************************************************************************
+
+# numbers in python are called integers********************************************
+# examples of integers are 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+# decimals in python are called floats
+# examples of floats are 1.0, 2.0, 3.0, 4.0, 5.0
+
+# negative numbers in python are called negative integers
+# examples of negative integers are -1, -2, -3, -4, -5
+
+# negative decimals in python are called negative floats
+# examples of negative floats are -1.0, -2.0, -3.0, -4.0, -5.0
+
+# using // in python will allow you to divide and round down
+#  example: 5 // 2 = 2
+
+# using % in python will allow you to divide and get the remainder
+#  example: 5 % 2 = 1
+
+# type gives you the type of the variable
+#  example: type(5) = int
+#  example: type(5.0) = float
+#  example: type("5") = str
+
+# using == in python will allow you to check if two things are equal
+#  example: 5 == 5 = True
+#  example: 5 == 6 = False
+#  example: 5 == "5" = False
+
+# a string is false if it is empty
+#  example: "" = False
+#  example: "hello" = True
+
+# boolean operators in python are and, or, not
+#  example: True and True = True
+#  example: True and False = False
+#  example: False and False = False
+#  example: True or True = True
+#  example: True or False = True
+#  example: False or False = False
+#  example: not True = False
+#  example: not False = True
+
+# using > in python will allow you to check if something is greater than
+#  example: 5 > 2 = True
+#  example: 2 > 5 = False
+#  example: 5 > 5 = False
+
+#  using >= in python will allow you to check if something is greater than or equal to
+#  example: 5 >= 2 = True
+#  example: 2 >= 5 = False
+#  example: 5 >= 5 = True
+
+# you use booleans in if statements
+#  example: if True:
+#               print("Hello World")
+
+# you can use elif (else if) in python to check another condition if the first condition is false
+#  example: if True:
+#               print("Hello World")
+#           elif False:
+#               print("Goodbye World")
+
