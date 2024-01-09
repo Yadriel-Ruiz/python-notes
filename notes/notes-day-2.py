d874c8736381a753f9e8947659d74f6d0284d5b3
Yadriel <147550067+Yadriel-Ruiz@users.noreply.github.com>
2024-01-09 09:27:40 -0600

    cleaned file locations

---
+++ notes/notes-day-2.py
@@ -0,0 +1,10 @@
+# Tools and Resources
+# Notion - https://www.notion.so/
+#Obsidian - https://obsidian.md/
+#Evernote - https://evernote.com/
+#Excalidraw - https://excalidraw.com/ (Diagramming)
+#Anki - https://apps.ankiweb.net/ (Vocabulary)
+
+# print("Hello World")
+#  Python is a program installed into your computer
+# use the terminal in order to run python
