5606e6898fa5476d234888af339815e1331f76c9
Yadriel <147550067+Yadriel-Ruiz@users.noreply.github.com>
2024-01-10 17:27:10 -0600

    functions notes

--- dictionary_exercises.py
+++ dictionary_exercises.py
@@ -2,7 +2,8 @@
 continents = {
     "Asia": ["China", "Mongolia", "India"],
     "South America": ["Brazil", "Argentina", "Chile"],
-    "North America": ["United States", "Canada", "Mexico"], "Antarctica": [],
+    "North America": ["United States", "Canada", "Mexico"],
+    "Antarctica": [],
     "Africa": ["South Africa", "Algeria", "Kenya", "Ethiopia", "Egypt"],
     "Europe": ["France", "Germany", "England", "Spain", "Greece", "Italy"],
     "Australia": ["New Zealand", "Australia", "Fiji"]
@@ -16,6 +17,7 @@ print(grab_fiji)
 sorted_continents = sorted(continents)
 print(sorted_continents)
 
+
 # Problem 2
 
 programmer = {"first_name": "Wally",
@@ -27,7 +29,9 @@ programmer = {"first_name": "Wally",
 print("The candidate, " + programmer["first_name"] + " " + programmer["last_name"] + " has \n" + str(
     programmer["years_experience"]) + " years of experience as a " + programmer["role"] + ".")
 
+
 # Problem 3
+
 my_dict = {'a': 1, 'b': 2, 'c': 3}
 square_dict = {}
 for key in my_dict:
@@ -38,5 +42,5 @@ new_dict = {}
 for key in my_dict:
     new_dict[my_dict[key]] = key
 my_dict = new_dict
-print("my_dict = " + str(new_dict))
+print("my_dict = " + str(my_dict))
 
